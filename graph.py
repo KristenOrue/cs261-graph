b209d67af062d9d54a7e169e258fb25cfe7e9afb
Kristen Orue <korue113@gmail.com>
2019-11-23 13:36:56 -0800

    Pass neigbors test

---
+++ graph.py
@@ -0,0 +1,21 @@
+# Graph: An efficient graph.
+# A graph implementation that uses an adjacency list to represent vertices
+# and edges.
+# Your implementation should pass the tests in test_graph.py.
+# KRISTEN ORUE
+
+import functools
+
+class Graph:
+
+    def __init__(self):
+        self.data = {}
+
+    def adjacent(self, Vertex1, Vertex2):
+        if self.data == {}:
+          return False
+    def neighbors(self, Vertex1):
+      return []
+
+    
+
